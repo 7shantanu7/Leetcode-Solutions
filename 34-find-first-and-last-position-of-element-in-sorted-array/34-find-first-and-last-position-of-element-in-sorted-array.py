d2f7f6dfacbef11ea1e4e562c7efe200f79c6ac5
Shantanu Zadbuke <51382138+7shantanu7@users.noreply.github.com>
2022-02-16 18:30:42 +0530

    Time: 179 ms (5.84%), Space: 15.4 MB (55.78%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,20 +1,25 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        def search(x):
-            lo, hi = 0, len(nums)           
-            while lo < hi:
-                mid = (lo + hi) // 2
-                if nums[mid] < x:
-                    lo = mid+1
-                else:
-                    hi = mid                    
-            return lo
-        
-        lo = search(target)
-        hi = search(target+1)-1
-        
-        if lo <= hi:
-            return [lo, hi]
-                
-        return [-1, -1]
+class Solution :
+    def  searchRange(self, nums,  target) :
+        ans = [-1, -1]
+        ans[0] = self.search(nums, target, True)
+        if (ans[0] != -1) :
+            ans[1] = self.search(nums, target, False)
+        return ans
+    def  search(self, nums,  target,  findStart):
+        ans = -1
+        start = 0
+        end = len(nums) - 1
+        while (start <= end) :
+            mid = start + int((end - start) / 2)
+            if (target < nums[mid]) :
+                end = mid - 1
+            elif(target > nums[mid]) :
+                    start = mid + 1
+            else :
+                    #  potential ans found
+                ans = mid
+                if (findStart) :
+                    end = mid - 1
+                else :
+                    start = mid + 1
+        return ans
